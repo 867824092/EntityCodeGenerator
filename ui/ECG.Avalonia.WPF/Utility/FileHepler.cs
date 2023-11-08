546726c19d435cf0bc3b4c8d3bb1c33c6a43ca6e
snake.lin <snake.lin@ly.com>
2023-11-08 15:44:19 +0800

    写入文件默认为utf8格式

--- ui/ECG.Avalonia.WPF/Utility/FileHepler.cs
+++ ui/ECG.Avalonia.WPF/Utility/FileHepler.cs
@@ -13,7 +13,7 @@ public class FileHepler {
     // 如果文件不存在则创建，如果存在则覆盖，异步方法
     public static async Task CreateFileIfNotExistsAsync(string path, string content) {
         await using var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
-        await using var writer = new StreamWriter(stream);
+        await using var writer = new StreamWriter(stream,System.Text.Encoding.UTF8);
         await writer.WriteAsync(content);
     }
 }
